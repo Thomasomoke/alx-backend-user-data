e58a2d6b6ebd59caba52874966672a784dc1dbdc
Thomasomoke <omokethomas644@gmail.com>
2024-11-12 16:45:57 +0300

    forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from typing import Literal
 
 
 app = Flask(__name__)
@@ -23,13 +24,13 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized(error) -> tuple[str, Literal[401]]:
     """ Not found handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(403)
-def Forbidden(error) -> str:
+def Forbidden(error) -> tuple[str, Literal[403]]:
     """ Not found handler
     """
     return jsonify({"error": "Forbidden"}), 403
