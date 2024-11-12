bd1404f89736a1f31d72b7c80e0bf0297d84c364
Thomasomoke <omokethomas644@gmail.com>
2024-11-12 17:46:18 +0300

    Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,24 +8,41 @@ from typing import List
 class Auth:
     """ a class to manage the API authentication.
     """
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """a function that returns True if the path is not in the list of
         """
+        if path and not path.endswith("/"):
+            path += "/"
+        if not path or path not in excluded_paths:
+            return True
+        if not path:
+            return True
+        if not excluded_paths or excluded_paths == []:
+            return True
+        if path is excluded_paths:
+            return False
         return False
-    
+
     def authorization_header(self, request=None) -> None:
         """function that handles authorization header
         """
         return None
-    
-    def current_user(self, request=None) ->None:
+
+    def current_user(self, request=None) -> None:
         """function that handles current user
         """
         return None
-    
+
+
 if __name__ == "__main__":
     a = Auth()
-    print(a.require_auth("/api/v1/status", ["/api/v1/status"]))
-    print(a.authorization_header())
-    print(a.current_user())
+
+    print(a.require_auth(None, None))
+    print(a.require_auth(None, []))
+    print(a.require_auth("/api/v1/status/", []))
+    print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
+    print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
+    print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
+    print(a.require_auth
+          ("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
