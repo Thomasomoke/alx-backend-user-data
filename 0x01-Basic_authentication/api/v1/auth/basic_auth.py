09a136b2ee691778cffd502b3d9d435a24433932
Thomasomoke <omokethomas644@gmail.com>
2024-11-12 22:04:01 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,15 +2,15 @@
 """A class BasicAuth that inherits from Auth."""
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar, Optional
+
+User = TypeVar('User')
 
 
 class BasicAuth(Auth):
-    """Basic Authentication class."""
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """Extracts Base64 part from the Authorization header."""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -20,7 +20,6 @@ class BasicAuth(Auth):
         return authorization_header.split(" ")[1]
 
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
-        """Decodes a Base64 string."""
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):
@@ -32,7 +31,6 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
-        """Extracts user email and password from the decoded Base64 string."""
         if decoded_base64_authorization_header is None:
             return None, None
         if not isinstance(decoded_base64_authorization_header, str):
@@ -40,3 +38,17 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return decoded_base64_authorization_header.split(':', 1)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> Optional[User]:
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        user_instance = User.search(user_email)
+        if user_instance is None:
+            return None
+        if not user_instance.is_valid_password(user_pwd):
+            return None
+
+        return user_instance
