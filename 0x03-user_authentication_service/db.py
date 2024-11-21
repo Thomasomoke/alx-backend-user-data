cc69d6c158107ffd676d33f00cdabcef8082e580
Thomasomoke <omokethomas644@gmail.com>
2024-11-21 08:52:54 +0300

    create user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User  # Ensure User is imported
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database
+
+        Args:
+            email (str): The user's email address.
+            hashed_password (str): The user's hashed password.
+
+        Returns:
+            User: The created User object.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)  # Add the user to the session
+        self._session.commit()
+        return new_user  # Return the created User object
