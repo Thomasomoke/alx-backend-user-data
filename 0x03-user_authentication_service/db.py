ae1956b06f62f2680113143926bc328d854ed722
Thomasomoke <omokethomas644@gmail.com>
2024-11-21 09:54:43 +0300

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User  # Ensure User is imported
 
@@ -42,3 +43,17 @@ class DB:
         self._session.add(new_user)  # Add the user to the session
         self._session.commit()
         return new_user  # Return the created User object
+
+    def find_user_by(self, **kwargs):
+        session = self._session
+        valid_columns = {column.name for column in User.__table__.columns}
+        for key in kwargs.keys():
+            if key not in valid_columns:
+                raise InvalidRequestError(f"Invalid query argument: {key}")
+
+        user = session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound("No user found matching the criteria.")
+
+        return user
